f385bd8c6797bdc11391856657c9376f6b21ee91
Misheck12 <misheck1720@gmail.com>
2022-08-19 22:42:38 +0200

    Added Greeting component

--- app/controllers/v1/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class V1::GreetingsController < ApplicationController
-  def index
-    @message = Message.find(Random.rand(1..5))
-    render json: { messages: @message.message }.to_json
-  end
-end
